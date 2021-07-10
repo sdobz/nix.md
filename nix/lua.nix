c716957dedf454581d0f1ea8605216a856db72c7
sdobz <vincent@khougaz.com>
2021-07-10 13:13:44 -0700

    Installed lunamark markdown transformer

--- nix/lua.nix
+++ nix/lua.nix
@@ -39,6 +39,49 @@ let
     };
   });
 
-  luaDeps = with pkgs.luaPackages; [ tl ];
+
+  cosmo = pkgs.luaPackages.buildLuarocksPackage (with pkgs; with pkgs.luaPackages; rec {
+    pname = "cosmo";
+    version = "16.06.04-1";
+
+    src = fetchurl {
+      url    = "https://luarocks.org/${pname}-${version}.src.rock";
+      sha256 = "1adrk74j0x1yzhy0xz9k80hphxdjvm09kpwpbx00sk3kic6db0ww";
+    };
+    disabled = (luaOlder "5.1") || (luaAtLeast "5.4");
+    propagatedBuildInputs = [ lpeg ];
+
+    meta = with lib; {
+      homepage = "https://github.com/mascarenhas/cosmo";
+      description = "Lua library for conversion between markup formats ";
+      maintainers = with maintainers; [ mascarenhas ];
+      license.fullName = "MIT/X11";
+    };
+  });
+  lunamark = pkgs.luaPackages.buildLuarocksPackage (with pkgs; with pkgs.luaPackages; rec {
+    pname = "lunamark";
+    version = "0.5.0-1";
+
+    knownRockspec = (fetchurl {
+      url    = "https://luarocks.org/${pname}-${version}.rockspec";
+      sha256 = "0wrhs1qzw2jpc251akiv5vhxmz026jcs7s0p43x7lp41nmj1xx2p";
+    }).outPath;
+
+    src = fetchurl {
+      url    = "https://github.com/jgm/lunamark/archive/refs/tags/0.5.0.tar.gz";
+      sha256 = "05kpv3vp12kzsdwa0x6l3a35igyfk1b2pa6iv94c64c6m075jrva";
+    };
+    disabled = (luaOlder "5.1") || (luaAtLeast "5.4");
+    propagatedBuildInputs = [ lua alt-getopt cosmo lpeg luautf8 ];
+
+    meta = with lib; {
+      homepage = "https://github.com/jgm/lunamark";
+      description = "Lua library for conversion between markup formats ";
+      maintainers = with maintainers; [ jgm ];
+      license.fullName = "MIT/X11";
+    };
+  });
+
+  luaDeps = with pkgs.luaPackages; [ tl lunamark ];
 in
   luaDeps
